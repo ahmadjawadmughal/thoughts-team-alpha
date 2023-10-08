91a1b995ce132e92c9cd800c60ee27df60e88ef5
ahmadjawadmughal <ahmedjawwad02@gmail.com>
2023-10-08 06:48:36 -0700

    CRUD operations for both apps(users & thoughts)

---
+++ post_thoughts/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render
+from django.views.generic.edit import CreateView,UpdateView,FormView,DeleteView
+from django.views.generic import TemplateView, ListView, DetailView
+from post_thoughts.models import *
+# Create your views here.
+
+class CreateThoughts(CreateView):
+    model = Thought
+    fields = "__all__"
+    success_url = "/post_thoughts/Success/"
+
+class UpdateThought(UpdateView):
+    model = Thought
+    fields = "__all__"
+    success_url = "/post_thoughts/Success/"
+
+class DeleteThoughts(DeleteView):
+    model = Thought
+    fields = "__all__"
+    success_url = "/post_thoughts/Success/"
+
+class ListThoughts(ListView):
+    model = Thought
+
+    success_url = "/post_thoughts/Success/"
+
+class DetailThoughts(DetailView):
+    model = Thought
+
+    success_url = "/post_thoughts/Success/"        
+
+class Success(TemplateView):
+    template_name = "post_thoughts/success.html/"
+
+# CRUD for the Comment Model
+
+
+class CreateComment(CreateView):
+    model = Comment
+    fields = "__all__"
+
+    success_url = "/thoughts/SuccessComment/"
+
+class UpdateComment(UpdateView):
+    model = Comment
+    fields = "__all__"
+
+    success_url = "/thoughts/SuccessComment/"
+
+class DeleteComment(DeleteView):
+    model = Comment
+    fields = "__all__"
+
+    success_url = "/thoughts/SuccessComment/"
+
+class ListComment(ListView):
+     model = Comment
+
+     success_url = "/thoughts/SuccessComment/"
+
+class DetailComment(DetailView):
+    model = Comment
+
+    success_url = "/thoughts/SuccessComment/"             
+
+class SuccessComment(TemplateView):
+
+    template_name = "post_thoughts/success.html/"
