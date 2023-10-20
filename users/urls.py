fc8054ae1606edd8cdd9d599b9188a3d73ddcaae
Ahmad Jawad Mughal <127230519+ahmadjawadmughal@users.noreply.github.com>
2023-10-20 09:14:55 -0700

    Final Project Done

--- users/urls.py
+++ users/urls.py
@@ -7,11 +7,8 @@ urlpatterns = [
     path("login/",user_login, name="login"),
     path("logout/",user_logout, name="logout"),
     path("Success/",Success.as_view(),name="Success"),
-    #path("ConfirmDeleteProfie/",Confirm_delete_profile.as_view(),name="ConfirmDeleteProfie"),
 
     path("ChangePass/", Changepass.as_view(), name="ChangePassword"),
-    # logged in user profile detail
-    path("myprofile/<pk>/",myProfile_detail.as_view(),name="myProfile_detail"),
 
     # CRUD
     path("profile/",Profile.as_view(), name="Profile"),
